8409effd7b9bd5183a0d1b1d21857029d9d3d287
vnool <goodook@163.com>
2015-03-02 17:42:23 +0800

    test

--- cloud/app.js
+++ cloud/app.js
@@ -8,7 +8,7 @@ app.set('view engine', 'ejs');    // 设置 template 引擎
 app.use(express.bodyParser());    // 读取请求 body 的中间件
 
 var two=require("cloud/test.js");
-response.write( two.name);
+res.send(  two.name);
 
 // 使用 Express 路由 API 服务 /hello 的 HTTP GET 请求
 app.get('/hello', function(req, res) {
