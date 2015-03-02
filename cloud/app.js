0045688c6ad0f31fe116c4efcf91f37b4bae09a0
vnool <goodook@163.com>
2015-03-02 15:53:17 +0800

    test

--- cloud/app.js
+++ cloud/app.js
@@ -9,8 +9,20 @@ app.use(express.bodyParser());    // 读取请求 body 的中间件
 
 // 使用 Express 路由 API 服务 /hello 的 HTTP GET 请求
 app.get('/hello', function(req, res) {
+	savetest();
+	
   res.render('hello', { message: 'Congrats, you just set up your app!' });
 });
 
+
+function savetest(){
+	var HELLO = AV.Object.extend("hello"); 
+	var hello = new HELLO(); 
+	 hello.set("name", 'xxxx');
+    hello.set("val", "Sean Plott");
+ 
+    hello.save(null);
+    
+}
 // 最后，必须有这行代码来使 express 响应 HTTP 请求
 app.listen();
