a93f52604a56be81bc34f757fa56976f47a7fe28
vnool <goodook@163.com>
2015-03-02 17:45:09 +0800

    test

--- cloud/app.js
+++ cloud/app.js
@@ -7,12 +7,14 @@ app.set('views','cloud/views');   // 设置模板目录
 app.set('view engine', 'ejs');    // 设置 template 引擎
 app.use(express.bodyParser());    // 读取请求 body 的中间件
 
-var two=require("cloud/test.js");
-res.send(  two.name);
+
 
 // 使用 Express 路由 API 服务 /hello 的 HTTP GET 请求
 app.get('/hello', function(req, res) {
-	  
+	  var two=require("cloud/test.js");
+      res.send(  two.name);
+
+
 		var HELLO = AV.Object.extend("hello"); 
 		var hello = new HELLO(); 
 		hello.set("name", 'xxxx');
