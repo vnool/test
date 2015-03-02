b275646413fdfe3252fb75125f8c446721447184
vnool <goodook@163.com>
2015-03-02 16:10:07 +0800

    etest

--- cloud/app.js
+++ cloud/app.js
@@ -9,14 +9,14 @@ app.use(express.bodyParser());    // 读取请求 body 的中间件
 
 // 使用 Express 路由 API 服务 /hello 的 HTTP GET 请求
 app.get('/hello', function(req, res) {
-	savetest();
+	
 	
      res.render('hello', { message: 'Congrats, you just set up your app!' });
 });
 
-
+savetest();
 function savetest(){
-	 response.success("Hello world!");
+	 
 	 
 	var HELLO = AV.Object.extend("hello"); 
 	var hello = new HELLO(); 
